f09ee289ddb65dbc6cc2d91bba84307997a75770
bill26-peach <bill73777@qq.com>
2026-04-28 10:18:08 +0800

    init

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -28,11 +28,7 @@ axios.defaults.headers['clientid'] = import.meta.env.VITE_APP_CLIENT_ID;
 // 创建 axios 实例
 const service = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_API,
-  timeout: 50000,
-  transitional: {
-    // 超时错误更明确
-    clarifyTimeoutError: true
-  }
+  timeout: 50000
 });
 
 // 请求拦截器
--- vite/plugins/icons.ts
+++ vite/plugins/icons.ts
@@ -3,7 +3,6 @@ import Icons from 'unplugin-icons/vite';
 export default () => {
   return Icons({
     // 自动安装图标库
-    autoInstall: true,
-    compiler: "vue3"
+    autoInstall: true
   });
 };
